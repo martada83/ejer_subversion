8d2c4ed2daa08ca49a09a907cf7cc6672817ddbf
martada83 <35797501+martada83@users.noreply.github.com>
2018-01-31 17:34:53 +0100

    Inicio das probas

--- src/testrefactorizacion/cliente.java
+++ src/testrefactorizacion/cliente.java
@@ -9,14 +9,10 @@ package testrefactorizacion;
  *
  * @author Marta
  */
-public class TestRefactorizacion {
+class cliente {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        cliente c=new cliente("IES de Rodeira");
+    cliente(String ies_de_Rodeira) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
